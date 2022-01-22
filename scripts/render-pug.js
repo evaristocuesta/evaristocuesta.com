c8de2e8c7b11e70d3f98a946fddd3aacdcd33abd
evaristocuesta <evaristocg@gmail.com>
2022-01-22 15:13:49 +0100

    Add  info read from webconfig.json to index.html

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -1,19 +1,23 @@
 'use strict';
 const fs = require('fs');
 const upath = require('upath');
+const path = require('path');
 const pug = require('pug');
 const sh = require('shelljs');
 const prettier = require('prettier');
+const webConfigJSON = require('../src/config/webconfig.json');
 
 module.exports = function renderPug(filePath) {
     const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, '.html');
     const srcPath = upath.resolve(upath.dirname(__filename), '../src');
-
-    console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
+    var filename = path.basename(destPath, '.html');
+    console.log(`### INFO: Rendering ${filePath} to ${destPath} - ${filename}`);
     const html = pug.renderFile(filePath, {
         doctype: 'html',
         filename: filePath,
-        basedir: srcPath
+        basedir: srcPath, 
+        title: webConfigJSON[filename].title, 
+        description: webConfigJSON[filename].description
     });
 
     const destPathDirname = upath.dirname(destPath);
