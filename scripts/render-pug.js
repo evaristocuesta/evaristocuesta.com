6c012f6ba6d9ea81dc8835eb6550b133aef08c3d
evaristocuesta <evaristocg@gmail.com>
2022-01-22 15:18:56 +0100

    Remove path require. Use upath instead

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -1,7 +1,6 @@
 'use strict';
 const fs = require('fs');
 const upath = require('upath');
-const path = require('path');
 const pug = require('pug');
 const sh = require('shelljs');
 const prettier = require('prettier');
@@ -10,7 +9,7 @@ const webConfigJSON = require('../src/config/webconfig.json');
 module.exports = function renderPug(filePath) {
     const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, '.html');
     const srcPath = upath.resolve(upath.dirname(__filename), '../src');
-    var filename = path.basename(destPath, '.html');
+    var filename = upath.basename(destPath, '.html');
     console.log(`### INFO: Rendering ${filePath} to ${destPath} - ${filename}`);
     const html = pug.renderFile(filePath, {
         doctype: 'html',
