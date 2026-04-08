0c386cd17178a0c876f0f51ce68583aa1e15fa7e
evaristocuesta <evaristocg@gmail.com>
2026-04-08 19:22:26 +0200

    Add AspNetStatic

--- evaristocuesta.com/Program.cs
+++ evaristocuesta.com/Program.cs
@@ -1,7 +1,26 @@
+using AspNetStatic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+if (args.HasSsgArg())
+{
+    builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
+        new StaticResourcesInfoProvider(
+            [
+                new PageResource("/"), 
+                new CssResource("/css/site.css"),
+                new CssResource("/evaristocuesta.com.styles.css"),
+                new JsResource("/js/site.js"), 
+                new BinResource("/images/favicon.ico"),
+                new BinResource("/images/meta-image.jpg"),
+                new BinResource("/images/profile.jpg"),
+                new BinResource("/images/sidebar-background.jpg")
+            ]
+        ));
+}
 
 var app = builder.Build();
 
@@ -25,5 +44,22 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+if (args.HasSsgArg())
+{
+    var outputPath = args.Length >= 2 ? $"{args[1]}" : string.Empty;
+
+    if (!Path.Exists(outputPath))
+    {
+        Console.WriteLine($"Creating directory {outputPath}");
+        Directory.CreateDirectory(outputPath);
+    }
+
+    Console.WriteLine($"Generating static content in {outputPath}");
+
+    app.GenerateStaticContent(outputPath,
+        alwaysDefaultFile: true,
+        exitWhenDone: true,
+        dontUpdateLinks: true);
+}
 
 app.Run();
